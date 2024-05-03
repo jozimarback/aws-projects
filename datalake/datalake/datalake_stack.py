d05a764ed9fa59c4a06d09da3938d1cd1424eac6
Jozimar Back <jozimar5@gmail.com>
2024-05-03 10:48:19 -0300

    creation of glue db and glue table

--- datalake/datalake/datalake_stack.py
+++ datalake/datalake/datalake_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     RemovalPolicy,
     Stack,
     # aws_sqs as sqs,
+    aws_glue as glue,
     aws_s3 as s3
 )
 from constructs import Construct
@@ -23,4 +24,37 @@ class DatalakeStack(Stack):
             versioned=False,
             removal_policy=RemovalPolicy.RETAIN
         )
+
+        glue_database = glue.CfnDatabase(
+            self, "datalake-aws-database",
+            catalog_id="058264114498",
+            database_input=glue.CfnDatabase.DatabaseInputProperty(
+                location_uri=f"s3://{datalake_bucket.bucket_name}/bronze",
+                name="datalake-aws-database"
+            )
+        )
+
+        # Define the Glue crawler
+        # glue_crawler = glue.CfnCrawler(
+        #     self, "datalake-aws-crawler",
+        #     role="arn:aws:iam::058264114498:role/service-role/AWSGlueServiceRole-my-role",
+        #     database_name=glue_database.database_input.name,
+        #     targets={"s3Targets": [{"path": f"s3://{datalake_bucket.bucket_name}/bronze/*"}]},
+        #     table_prefix="bronze-"
+        # )
+
+        glue_table = glue.CfnTable(
+            self, "bronze",
+            database_name=glue_database.database_input.name,
+            catalog_id=glue_database.catalog_id,
+            table_input=glue.CfnTable.TableInputProperty(
+                name="bronze",
+                storage_descriptor=glue.CfnTable.StorageDescriptorProperty(
+                    input_format="parquet",
+                    location=f"s3://{datalake_bucket.bucket_name}/bronze"
+                )
+            )
+        )
+
+        glue_table.node.add_dependency(glue_database)
 
